be196c8458b185aa194b6db7c2af75603992f462
milindasandaru <sams.senarath@gmail.com>
2025-03-14 10:39:32 +0530

    Update admin part
    Update admin part

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      
+     <h1 className='text-blue-500 font-light'>Hello world!</h1>
     </>
   )
 }
